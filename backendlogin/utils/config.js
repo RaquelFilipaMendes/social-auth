92bc8c49b6e623d3b173d684bcfaa076b6329e30
DESKTOP-0S4MAIP\rmendes <1160475@isep.ipp.pt>
2018-07-29 16:55:11 +0100

    Social authentication with React + RESTful API

---
+++ backendlogin/utils/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth' : {
+        'clientID'         : 'XXX',
+        'clientSecret'     : 'XXX',
+        'callbackURL'      : 'http://localhost:3001/auth/google/callback'
+    }
+};
